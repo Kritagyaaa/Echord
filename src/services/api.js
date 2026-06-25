0621084f94621c4023b7b2a3296371c26861720f
shubhamkatyan1324 <143383969+shubhamkatyan1324@users.noreply.github.com>
2026-06-25 13:59:40 +0530

    frontend connection

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,38 @@
-const API = "http://localhost:5000/api";
+// const API = "http://localhost:5000/api";
 
-export async function getSongStream(id) {
-    const res = await fetch(`${API}/songs/${id}/stream`);
+// export async function getSongStream(id) {
+//     const res = await fetch(`${API}/songs/${id}/stream`);
 
-    if (!res.ok) {
+//     if (!res.ok) {
+//         throw new Error("Failed to fetch stream");
+//     }
+
+//     return res.json();
+// }
+const API_URL = "http://localhost:5000/api";
+
+export async function getSongs() {
+    const response = await fetch(`${API_URL}/songs`);
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch songs");
+    }
+
+    const data = await response.json();
+
+    return data.songs;
+}
+
+export async function getSongStream(songId) {
+    const response = await fetch(
+        `${API_URL}/songs/${songId}/stream`
+    );
+
+    if (!response.ok) {
         throw new Error("Failed to fetch stream");
     }
 
-    return res.json();
+    const data = await response.json();
+
+    return data.streamUrl;
 }
